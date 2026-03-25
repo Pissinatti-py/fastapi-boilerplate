a2824aa5f679eed694eaf85d87e4ee04c16d0373
Pissinatti-py <marcus.vinicius.prestador@samplemed.com.br>
2026-03-25 16:57:04 -0300

    refactor: remove unused authentication method and clean up imports

--- src/services/users/manager.py
+++ src/services/users/manager.py
@@ -2,9 +2,8 @@ import uuid
 from typing import Optional
 
 from fastapi import Depends
-from fastapi_users import BaseUserManager, UUIDIDMixin, exceptions, models
+from fastapi_users import BaseUserManager, UUIDIDMixin
 from fastapi_users.models import UP
-from fastapi.security import OAuth2PasswordRequestForm
 
 from src.core.settings import settings
 from src.db.user_database import get_user_db
@@ -27,28 +26,6 @@ class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
         :rtype: Optional[UP]
         """
         return self.user_db.get_by_username(username)
-
-    async def authenticate(
-        self, credentials: OAuth2PasswordRequestForm,
-    ) -> models.UP | None:
-        try:
-            user = await self.get_by_username(credentials.username)
-        except exceptions.UserNotExists:
-            # Run the hasher to mitigate timing attack
-            # Inspired from Django: https://code.djangoproject.com/ticket/20760
-            self.password_helper.hash(credentials.password)
-            return None
-
-        verified, updated_password_hash = self.password_helper.verify_and_update(
-            credentials.password, user.hashed_password,
-        )
-        if not verified:
-            return None
-
-        if updated_password_hash is not None:
-            await self.user_db.update(user, {"hashed_password": updated_password_hash})
-
-        return user
 
 
 async def get_user_manager(user_db=Depends(get_user_db)):
